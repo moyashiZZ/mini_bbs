7f3840b3dd94a53f74ecbc7ba3d72c31eec6045a
moyashiZZ <zababamoyashi0809@gmail.com>
2019-01-10 22:27:42 +0900

    ログアウト、投稿詳細画面追加

--- index.php
+++ index.php
@@ -15,22 +15,51 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
 
 if (!empty($_POST)) {
   if ($_POST['message'] !== '') {
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message = $db->prepare('INSERT INTO posts 
+                             SET member_id=?, 
+                                 message=?, 
+                                 reply_message_id=?, 
+                                 created=NOW()');
     $message->execute(array(
       $member['id'],
-      $_POST['message']
-
+      $_POST['message'],
+      $_POST['reply_post_id']
     ));
     header('Location: index.php');
   }
 }
 
-$posts = $db->query(
+$page = $_REQUEST['page'];
+if ($page == '') {
+  $page = 1;
+}
+$page = max($page, 1);
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxPage);
+
+$start = ($page -1) * 5;
+
+$posts = $db->prepare(
              'SELECT m.name, m.picture, p.* 
               FROM members m, posts p 
               WHERE m.id=p.member_id 
               ORDER BY p.created 
-              DESC');
+              DESC
+              LIMIT ?,5');
+
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
+
+if (isset($_REQUEST['res'])) {
+  //返信の処理
+  $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id = p.member_id AND p.id=?');
+  $response->execute(array($_REQUEST['res']));
+  
+  $table=$response->fetch();
+  $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -54,8 +83,8 @@ $posts = $db->query(
       <dl>
         <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES));?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES));?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES));?>" />
         </dd>
       </dl>
       <div>
@@ -70,19 +99,31 @@ $posts = $db->query(
       width="48" height="48" 
       alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES));?>" />
     <p><?php print(htmlspecialchars($post['message'],ENT_QUOTES));?>
-      <span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES));?>）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'],ENT_QUOTES));?></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
-[<a href="delete.php?id="
+      <span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES));?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES));?>">Re</a>]</p>
+    <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id']))?>"><?php print(htmlspecialchars($post['created'],ENT_QUOTES));?></a>
+    <?php if ($post['reply_message_id'] > 0):?>
+<a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id']))?>">返信元のメッセージ</a>
+    <?php endif; ?>
+    <?php if($_SESSION['id'] == $post['member_id']):?>
+[<a href="delete.php?id=<?php print(htmlspecialchars($post['id']));?>"
 style="color: #F33;">削除</a>]
+    <?php endif; ?>
     </p>
     </div>
 <?php endforeach;?>
 
 <ul class="paging">
-<li><a href="index.php?page=">前のページへ</a></li>
-<li><a href="index.php?page=">次のページへ</a></li>
+<?php if($page > 1 ) :?>
+  <li><a href="index.php?page=<?php print($page-1)?>">前のページへ</a></li>
+<?php else :?>
+  <li>前のページへ</li>
+<?php endif; ?>
+
+<?php if($page < $maxPage):?>
+  <li><a href="index.php?page=<?php print($page+1)?>">次のページへ</a></li>
+<?php else:?>
+  <li>次のページへ</li>
+<?php endif;?>
 </ul>
   </div>
 </div>
